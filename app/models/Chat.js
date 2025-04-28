96109b828dce0cbe8e80d2786fa962db591d92a1
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-04-27 20:25:52 -0700

    update chat modle to include user and admin unread and also remove admin from model

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -3,13 +3,15 @@ const { Schema, default: mongoose } = require('mongoose');
 const chatSchema = new Schema({
     channel: { type: Schema.Types.ObjectId, ref: 'Channel', required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    admin: { type: Schema.Types.ObjectId, ref: 'User' },
+    // admin: { type: Schema.Types.ObjectId, ref: 'User' },
     lastMessage: { type: Schema.Types.ObjectId, ref: 'Message' },
     messages: [{ 
         type: Schema.Types.ObjectId,
         ref: 'Message'
     }],
     unreadCount: { type: Number, default: 0 },
+    userUnreadCount: { type: Number, default: 0 },
+    adminUnreadCount: { type: Number, default: 0 },
     isActive: { type: Boolean, default: true },
     startedAt: { type: Date, default: Date.now },
     endedAt: { type: Date },
