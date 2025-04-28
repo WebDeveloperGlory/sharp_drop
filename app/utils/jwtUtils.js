917374c04fd5b16ef822c6b38359394b3ca587aa
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-04-27 20:26:37 -0700

    add role to jwt instead of username

--- app/utils/jwtUtils.js
+++ app/utils/jwtUtils.js
@@ -4,13 +4,13 @@ const { JWT_SECRET } = require('../config/env');
 const secretKey = JWT_SECRET;
 
 const generateToken = ( user ) => {
-    const { _id, email, username } = user;
+    const { _id, email, role } = user;
     const maxAge = 24 * 60 * 60;
 
     return jwt.sign({
         userId: _id,
         email,
-        username
+        role
     }, secretKey, { 
         expiresIn: maxAge 
     });
