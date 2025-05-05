5da2cdb5cc594478ff5b2131a879b9546600c43f
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-05-05 13:18:19 +0100

    Update messageRoutes.js to use the upload middleware from fileUpload

--- app/routes/messageRoutes.js
+++ app/routes/messageRoutes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const controller = require('../controllers/messageController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
+const upload = require('../middlewares/fileUpload');
 
 const router = Router();
 
 // GENERAL ROUTES //
 router.post( '/text', authenticateUser, controller.sendTextMessage );
-router.post( '/image', authenticateUser, controller.sendImageMessage );
+router.post( '/image', authenticateUser, upload.single('imageFile'), controller.sendImageMessage );
 router.post( '/video', authenticateUser, controller.sendVideoMessage );
 router.put( '/:chatId/read', authenticateUser, controller.markAsRead );
 // END OF GENERAL ROUTES //
