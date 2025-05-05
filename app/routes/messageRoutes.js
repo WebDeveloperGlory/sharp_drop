4839274d6effe04b469c1ed5b0220d44f3e73be0
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-05-05 13:22:24 +0100

    Update messageRoutes.js

--- app/routes/messageRoutes.js
+++ app/routes/messageRoutes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 // GENERAL ROUTES //
 router.post( '/text', authenticateUser, controller.sendTextMessage );
-router.post( '/image', authenticateUser, upload.single('file'), controller.sendImageMessage );
+router.post( '/image', authenticateUser, upload.single('imageFile'), controller.sendImageMessage );
 router.post( '/video', authenticateUser, controller.sendVideoMessage );
 router.put( '/:chatId/read', authenticateUser, controller.markAsRead );
 // END OF GENERAL ROUTES //
