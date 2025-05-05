20e01c0365264c7e4fcb66ea3cf58eff29f3bef6
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-05-05 15:37:47 +0100

    Update messageRoutes.js

--- app/routes/messageRoutes.js
+++ app/routes/messageRoutes.js
@@ -2,12 +2,13 @@ const { Router } = require('express');
 const controller = require('../controllers/messageController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
 const upload = require('../middlewares/fileUpload');
+const uploadErrorHandler = require('../middlewares/uploadErrorHandler');
 
 const router = Router();
 
 // GENERAL ROUTES //
 router.post( '/text', authenticateUser, controller.sendTextMessage );
-router.post( '/image', authenticateUser, upload.single('imageFile'), controller.sendImageMessage );
+router.post( '/image', authenticateUser, upload.single('imageFile'), uploadErrorHandler, controller.sendImageMessage );
 router.post( '/video', authenticateUser, controller.sendVideoMessage );
 router.put( '/:chatId/read', authenticateUser, controller.markAsRead );
 // END OF GENERAL ROUTES //
