e0fde65c0366a40bbb12852eb6eae8387165daca
Umuerri Glory <104975242+WebDeveloperGlory@users.noreply.github.com>
2025-05-13 04:39:14 +0100

    Update userRoutes.js to include the delete routes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const controller = require('../controllers/userController');
 const { authenticateUser } = require('../middlewares/authMiddlewares');
+const { hasAdminPermissions } = require('../middlewares/adminMiddlewares');
 
 const router = Router();
 
 // GENERAL ROUTES //
 router.get( '/profile', authenticateUser, controller.getUserProfile );
+router.delete( '/profile/delete', authenticateUser, controller.deleteSelf );
+router.delete( '/profile/delete/:userId', authenticateUser, hasAdminPermissions, controller.deleteUser );
 router.get( '/referrals/personal', authenticateUser, controller.getUserReferrals );
 router.post( '/referrals/generate', authenticateUser, controller.generateReferralCode );
 router.post( '/referrals/validate/:referralCode', controller.validateReferralCode );
